0328677e1ea4af2995167896ad26266067ae4de5
Zeno Laiti <zeno.laiti@gmail.com>
2020-09-29 10:58:49 +0200

    Fixed DbContext

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -1,6 +1,8 @@
 using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,11 +10,22 @@ namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(opt =>
+                opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
+            
             services.AddControllers();
+            
             services.AddScoped<ICommandApiRepo, MockCommandApiRepo>();
         }
 
